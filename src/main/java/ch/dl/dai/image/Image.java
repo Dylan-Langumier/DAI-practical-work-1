c8a6d2d9fd2b9cab1de52b01e5c9d7e869f3b638
Raphael_Perret <87780955+rp2709@users.noreply.github.com>
2024-10-04 11:17:31 +0200

    Implement class image (#8)
    * implemented all basic functionalities of image class
    
    * implemented all basic functionalities of image class
    
    * Update src/main/java/ch/dl/dai/image/Image.java
    
    I'm stupid
    
    Co-authored-by: Dylan L. <dylan.langumie@hes-so.ch>
    
    ---------
    
    Co-authored-by: Dylan L. <dylan.langumie@hes-so.ch>

--- src/main/java/ch/dl/dai/image/Image.java
+++ src/main/java/ch/dl/dai/image/Image.java
@@ -1,4 +1,35 @@
 package ch.dl.dai.image;
 
+import java.awt.Color;
+
+/**
+ A simple array of colors representing an image
+ */
 public class Image {
+    private Color[] pixels;
+    private int width, height;
+
+    public Image(Color[] pixels, int width, int height) {
+        if(width * height != pixels.length){
+            throw new IllegalArgumentException("Dimensions don't match");
+        }
+        this.pixels = pixels;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+
+    public Color getPixel(int x, int y) {
+        return pixels[y * width + x];
+    }
+
+    public void setPixel(int x, int y, Color c) {
+        pixels[y * width + x] = c;
+    }
 }
