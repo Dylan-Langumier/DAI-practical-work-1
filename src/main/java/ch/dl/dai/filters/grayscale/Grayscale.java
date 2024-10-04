4556f1f61274df55b8062263595b5fa71bfd7b38
Dylan L <dylan.langumie@hes-so.ch>
2024-10-04 10:30:46 +0200

    Interface and overrides

---
+++ src/main/java/ch/dl/dai/filters/grayscale/Grayscale.java
@@ -0,0 +1,11 @@
+package ch.dl.dai.filters.grayscale;
+
+import ch.dl.dai.filters.ImageFilter;
+
+public class Grayscale implements ImageFilter {
+    @Override
+    public void applyFilter() {
+
+    }
+
+}
---
+++ src/main/java/ch/dl/dai/filters/movingaverage/MovingAverage.java
@@ -0,0 +1,11 @@
+package ch.dl.dai.filters.movingaverage;
+
+import ch.dl.dai.filters.ImageFilter;
+
+public class MovingAverage implements ImageFilter {
+    @Override
+    public void applyFilter() {
+
+    }
+
+}
