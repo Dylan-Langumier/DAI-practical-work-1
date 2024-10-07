c14caea12ceb8ddc32dbd0698f9a0cdeec936bc9
rp_2709 <rp2709@hotmail.com>
2024-10-07 23:34:27 +0200

    added an image argument to applyFilter

--- src/main/java/ch/dl/dai/filters/movingaverage/MovingAverage.java
+++ src/main/java/ch/dl/dai/filters/movingaverage/MovingAverage.java
@@ -1,11 +1,40 @@
 package ch.dl.dai.filters.movingaverage;
 
 import ch.dl.dai.filters.ImageFilter;
+import ch.dl.dai.image.Image;
+
+import java.awt.Color;
 
 public class MovingAverage implements ImageFilter {
     @Override
-    public void applyFilter() {
+    public void applyFilter(Image image) {
+        if (image.getHeight() < 3 || image.getWidth() < 3) {
+            throw new IllegalArgumentException("Image must be at least 3 x 3 to apply this filter");
+        }
+        Image referenceCopy = new Image(image);
+        for (int x = 1; x < image.getWidth() - 1; ++x) {
+            for (int y = 1; y < image.getHeight() - 1; ++y) {
+                image.setPixel(x, y, average(referenceCopy, x, y));
+            }
+        }
+    }
 
+    private static Color average(Image image, int x, int y) {
+        int red = 0, green = 0, blue = 0, alpha = 0;
+        for (int i = x - 1; i <= x + 1; ++i) {
+            for (int j = y - 1; j <= y + 1; ++j) {
+                Color pixel = image.getPixel(i, j);
+                red += pixel.getRed();
+                green += pixel.getGreen();
+                blue += pixel.getBlue();
+                alpha += pixel.getAlpha();
+            }
+        }
+        red /= 9;
+        green /= 9;
+        blue /= 9;
+        alpha /= 9;
+        return new Color(red, green, blue, alpha);
     }
 
 }
--- src/main/java/ch/dl/dai/filters/sepia/Sepia.java
+++ src/main/java/ch/dl/dai/filters/sepia/Sepia.java
@@ -1,10 +1,11 @@
 package ch.dl.dai.filters.sepia;
 
 import ch.dl.dai.filters.ImageFilter;
+import ch.dl.dai.image.Image;
 
 public class Sepia implements ImageFilter {
     @Override
-    public void applyFilter() {
+    public void applyFilter(Image image) {
 
     }
 
