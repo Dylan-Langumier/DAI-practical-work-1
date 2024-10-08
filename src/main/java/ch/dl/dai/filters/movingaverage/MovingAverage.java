e201fbb05f57b145f413ea4c4ca1b40270a62c18
rp_2709 <rp2709@hotmail.com>
2024-10-08 14:33:28 +0200

    corrected MovingAverage filter to also smooth borders and added the possibility to adjust the size of the average sample

--- src/main/java/ch/dl/dai/filters/movingaverage/MovingAverage.java
+++ src/main/java/ch/dl/dai/filters/movingaverage/MovingAverage.java
@@ -8,21 +8,28 @@ import java.awt.Color;
 public class MovingAverage implements ImageFilter {
     @Override
     public void applyFilter(Image image) {
-        if (image.getHeight() < 3 || image.getWidth() < 3) {
-            throw new IllegalArgumentException("Image must be at least 3 x 3 to apply this filter");
-        }
         Image referenceCopy = new Image(image);
-        for (int x = 1; x < image.getWidth() - 1; ++x) {
-            for (int y = 1; y < image.getHeight() - 1; ++y) {
+        for (int x = 0; x < image.getWidth(); ++x) {
+            for (int y = 0; y < image.getHeight(); ++y) {
                 image.setPixel(x, y, average(referenceCopy, x, y));
             }
         }
     }
 
     private static Color average(Image image, int x, int y) {
+        final int DEFAULT = 3;
+        return average(image,x,y,DEFAULT);
+    }
+
+    private static Color average(Image image, int x, int y, int squareSize) {
+        int halfSize = squareSize / 2;
         int red = 0, green = 0, blue = 0, alpha = 0;
-        for (int i = x - 1; i <= x + 1; ++i) {
-            for (int j = y - 1; j <= y + 1; ++j) {
+        int nbAveragedPixels = 0;
+        for (int i = x - halfSize; i <= x + halfSize; ++i) {
+            for (int j = y - halfSize; j <= y + halfSize; ++j) {
+                if(i < 0 || i >= image.getWidth() || j < 0 || j >= image.getHeight())
+                    continue;
+                ++nbAveragedPixels;
                 Color pixel = image.getPixel(i, j);
                 red += pixel.getRed();
                 green += pixel.getGreen();
@@ -30,10 +37,10 @@ public class MovingAverage implements ImageFilter {
                 alpha += pixel.getAlpha();
             }
         }
-        red /= 9;
-        green /= 9;
-        blue /= 9;
-        alpha /= 9;
+        red /= nbAveragedPixels;
+        green /= nbAveragedPixels;
+        blue /= nbAveragedPixels;
+        alpha /= nbAveragedPixels;
         return new Color(red, green, blue, alpha);
     }
 
