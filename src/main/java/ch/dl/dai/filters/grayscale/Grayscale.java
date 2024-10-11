c391714e311a4ed0b39bce81e9cde0064d1ff4e5
Dylan L. <dylan.langumie@hes-so.ch>
2024-10-11 10:00:25 +0200

    Add grayscale (#14)

--- src/main/java/ch/dl/dai/filters/grayscale/Grayscale.java
+++ src/main/java/ch/dl/dai/filters/grayscale/Grayscale.java
@@ -2,8 +2,18 @@ package ch.dl.dai.filters.grayscale;
 
 import ch.dl.dai.filters.ImageFilter;
 import ch.dl.dai.image.Image;
+import java.awt.Color;
 
 public class Grayscale implements ImageFilter {
   @Override
-  public void applyFilter(Image image) {}
+  public void applyFilter(Image image) {
+    for (int x = 0; x < image.getWidth(); ++x) {
+      for (int y = 0; y < image.getHeight(); ++y) {
+        Color c = image.getPixel(x, y);
+        int average = (c.getRed() + c.getGreen() + c.getBlue()) / 3;
+        Color gray = new Color(average, average, average);
+        image.setPixel(x, y, gray);
+      }
+    }
+  }
 }
